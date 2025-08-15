2e305dabf2e0aaa60b041e6af358b280ef722fa7
sebmaz <seb.maz93@icloud.com>
2025-08-16 02:14:38 +0400

    Add go lang files for token

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sebmaz93/interpreter
+
+go 1.25.0
---
+++ src/token/token.go
@@ -0,0 +1,25 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Kind    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL   = "ILLEGAL"
+	EOF       = "EOF"
+	IDENT     = "IDENT"
+	INT       = "INT"
+	ASSIGN    = "="
+	PLUS      = "+"
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	FUNCTION  = "FUNCTION"
+	LET       = "LET"
+)
